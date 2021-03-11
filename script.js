300c62a878c35ff4a33641ff16e67fea44153bf2
Gideon Berridge <gid.berridge@gmail.com>
2021-03-11 19:47:02 +0000

    Javascript modified

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-console.log("testing...");
+//console.log("testing...");
 
-const cheer = new Audio("audio/cheer.wav");
-const boo = new Audio("audio/boohiss.wav");
+const cheer = new Audio("Sounds/cheer.wav");
+const boo = new Audio("Sounds/boohiss.wav");
+const correct = document.querySelector(".correctAnswer");
+const answerText = document.querySelector('.answerText');
+const wrong = document.querySelector(".wrongAnswer");
 
 // if question is correct play cheer sound effect, display anser and change bg colour
 
-const correctTasks = function() {
-    qtextbox.classList.remove("hidden");
-    qtextbox.classList.add("correct");
-    cheer.play();
-};
+correct.addEventListener('click', function() {
+    answerText.classList.remove("hidden");
+    correct.classList.add("correct");
+    //cheer.play();
 
-//if answer is wrong play boo sound effect and change bg colour
+    //if answer is wrong play boo sound effect and change bg colour
 
-const wrong = function() {
-    qtextbox.classList.add("wrong");
-    boo.play();
-};
+    wrong.addEventListener('click', function() {
+        wrong.classList.add("wrong");
+        console.log('test')
+            //boo.play();
+    });
 
-//assign correct and wrong to answers
-const correct = document.querySelector(".correct");
+    //assign correct and wrong to answers
 
-correct.addEventListener("click", correctTasks);
+
+
+
+});
